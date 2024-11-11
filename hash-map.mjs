f8df89dd7f9f8cf1910a316563751b89c9d068c9
Lancer385 <ahmedkhalaf1234123@gmail.com>
2024-11-11 19:17:30 +0200

    adjust load factor resizing, added clear(), keys/values retrieval, implemented remove(key) and has(key) methods

--- hash-map.mjs
+++ hash-map.mjs
@@ -12,15 +12,13 @@ export class HashMap{
       for (let i = 0; i < key.length; i++) {
         hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % (this.buckets.length);
       }
-      console.log(`Hash for key '${key}': ${hashCode}`);  // <-- debug line!
-      console.log(`bucket length: ${this.buckets.length}`);
+      console.log(`Hash for key '${key}': ${hashCode}`); 
       return hashCode;
     }
     set(key, value){
-      if (this.size / this.buckets.length > 0.70){
+      if (this.size / this.buckets.length >= 0.75){
         this.resize();
       }
-
       const node = new Node(key, value);
       const hashCode = this.hash(key);
       if (this.buckets[hashCode] === undefined){
@@ -41,14 +39,13 @@ export class HashMap{
       }
       temp.next = node;
       this.size++;
-      console.log(this.size / this.buckets.length);
       return;
   };
   resize(){
     let oldBucket = this.buckets;
     this.buckets = new Array((oldBucket.length * 2));
     this.size = 0;
-    
+
     for (let i = 0; i < oldBucket.length; i++){
       if (oldBucket[i] !== undefined){
         this.set(oldBucket[i].key, oldBucket[i].value)
@@ -62,5 +59,115 @@ export class HashMap{
       }
     }
   }
-    
+  get(key){
+    for (let i = 0; i < this.buckets.length; i++){
+      if (this.buckets[i] !== undefined){
+        if (this.buckets[i].key === key){
+          return this.buckets[i].value;
+        }
+        if (this.buckets[i].next !== null){
+          let temp = this.buckets[i];
+          while(temp.next !== null){
+            temp = temp.next;
+            if (temp.key === key){
+              return temp.value;
+            }
+          }
+        }
+      }
+    }
+    return null;
+  }
+  has(key){
+    if (this.get(key) === null){
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  remove(key){
+    for (let i = 0; i < this.buckets.length; i++){
+      if (this.buckets[i] !== undefined){
+        if (this.buckets[i].key === key){
+          if (this.buckets[i].next !== null){
+            this.buckets[i] = this.buckets[i].next;
+            return true;
+          }
+          delete this.buckets[i];
+          this.size--;
+          return true;
+        }
+
+        if (this.buckets[i].next !== null){
+          let prev;
+          let temp = this.buckets[i];
+          while (temp.next !== null){
+            prev = temp;
+            temp = temp.next;
+            if (temp.key === key){
+                prev.next = temp.next;
+                return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  clear(){
+    let length = this.buckets.length;
+    this.buckets = new Array(length);
+    this.size = 0;
+  }
+
+  keys(){
+    let keyArray = [];
+    for (let i = 0; i < this.buckets.length; i++){
+      if (this.buckets[i] !== undefined){
+        keyArray.push(this.buckets[i].key);
+        if (this.buckets[i].next !== null){
+          let temp = this.buckets[i];
+          while (temp.next !== null){
+            temp = temp.next;
+            keyArray.push(temp.key);
+          }
+        }
+      }
+    }
+    return keyArray;
+  }
+  values(){
+    let valueArray = [];
+    for (let i = 0; i < this.buckets.length; i++){
+      if (this.buckets[i] !== undefined){
+        valueArray.push(this.buckets[i].value);
+        if (this.buckets[i].next !== null){
+          let temp = this.buckets[i];
+          while (temp.next !== null){
+            temp = temp.next;
+            valueArray.push(temp.value);
+          }
+        }
+      }
+    }
+    return valueArray;
+  }
+  entries(){
+    let entryArray = [];
+    for (let i = 0; i < this.buckets.length; i++){
+      if (this.buckets[i] !== undefined){
+        entryArray.push([this.buckets[i].key, this.buckets[i].value]);
+        if (this.buckets[i].next !== null){
+          let temp = this.buckets[i];
+          while (temp.next !== null){
+            temp = temp.next;
+            entryArray.push([temp.key, temp.value]);
+          }
+        }
+      }
+    }
+    return entryArray;
+  }
 };
