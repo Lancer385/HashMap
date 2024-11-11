8b4ab20b6e639ec59d4713b7b7a251e68f1cad25
Lancer385 <ahmedkhalaf1234123@gmail.com>
2024-11-11 19:58:57 +0200

    add tests for HashSet class

--- test.mjs
+++ test.mjs
@@ -1,7 +1,8 @@
 import { HashMap } from "./hash-map.mjs";
+import { HashSet } from "./hash-set.mjs";
 
 const test = new HashMap();
-
+const test2 = new HashSet();
 
 test.set('apple', 'red')
 test.set('banana', 'yellow')
@@ -36,3 +37,36 @@ test.set('moon', 'silver')
 test.remove('moon')
 console.log(test.entries())
 
+
+test2.set('apple')
+test2.set('banana')
+test2.set('carrot')
+test2.set('dog')
+test2.set('elephant')
+test2.set('frog')
+test2.set('grape')
+test2.set('hat')
+test2.set('ice cream')
+test2.set('jacket')
+test2.set('kite')
+test2.set('lion')
+test2.set('act')
+
+
+console.log(test2.has('apple')); // red
+console.log(test2.has('banana')); // yellow
+console.log(test2.has('carrot')); // orange
+console.log(test2.has('dog')); // brown
+console.log(test2.has('elephant')); // gray
+console.log(test2.has('frog')); // green
+console.log(test2.has('grape')); // purple
+console.log(test2.has('hat')); // black
+console.log(test2.has('ice cream')); // white
+console.log(test2.has('jacket')); // blue
+console.log(test2.has('kite')); // pink
+console.log(test2.has('lion')); // golden
+console.log(test2.has('act')); // who
+
+test2.remove('act')
+
+console.log(test2.keys())
